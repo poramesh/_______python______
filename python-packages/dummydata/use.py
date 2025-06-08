2a31c9d13272aaea0b82162e0dbd9c90d3ad4679
Pooja Jinnagara Ramesh <poojajramesh@gmail.com>
2025-06-08 16:46:39 +0100

    packaging

--- python-packages/dummydata/use.py
+++ python-packages/dummydata/use.py
@@ -1,4 +1,4 @@
-from fake_module import dummy_data_generator
+from dummy_data_builder import dummy_data_generator
 
 faker = dummy_data_generator()
 print("OKAY IA M GETTIGN PRINTED AND I DID IT",faker.name())
